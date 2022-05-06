11f0c3b13e993b046f5ce5ecdb34ed120a661c9b
Noah Buchanan <56573280+Noah6544@users.noreply.github.com>
2022-05-06 14:16:57 -0400

    updated timer to be 1 minute and not 1 second
    5 seconds was implemented just to check winscreen.

--- Scoreboard.py
+++ Scoreboard.py
@@ -5,7 +5,7 @@ class Scoreboard:
     def __init__(self):
         self.font = pygame.font.SysFont("sitkasmallsitkatextitalicsitkasubheadingitalicsitkaheadingitalicsitkadisplayitalicsitkabanneritalic",50)
         self.clock_font = pygame.font.SysFont("franklingothicmedium",100)
-        self.counter = 5
+        self.counter = 60
         self.text_counter = str(self.counter)
         self.render_font = self.clock_font.render(str(self.text_counter), True, (255, 255, 255))
         self.clock_render_font = self.clock_font.render(str(self.text_counter), True, (0, 0, 0))
